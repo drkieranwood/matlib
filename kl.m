63dc41219dc03cbcf595b84930177d63f04e2dd3
Kieran Wood <kieranwood85@yahoo.co.uk>
2013-09-28 21:11:53 +0100

    Added width and heigh quick functions

---
+++ kl.m
@@ -0,0 +1,5 @@
+function [ output_args ] = kl( input_args )
+%KL Returns the length of a matrix
+[output_args,~]=size(input_args);
+end
+
---
+++ kw.m
@@ -0,0 +1,5 @@
+function [ output_args ] = kw( input_args )
+%KW Returns the width of a matrix
+[~,output_args]=size(input_args);
+end
+
